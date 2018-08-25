5b4b50d9d7f85b9f86ab0715b3afeb29cf577277
Nicholas Karlsen <nichoka@student.matnat.uio.no>
2018-08-25 11:17:46 +0200

    End of session

--- Project_1/code/main.py
+++ Project_1/code/main.py
@@ -1,13 +1,20 @@
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+
+# Initial Conditions & parameters
+n = 1e3
+h = 1.0 / float(n)
+u0 = 0  # u(0) = 0
+u1 = 0  # u(1) = 0
+x = np.linspace(0, 1, h)
 
 
-def diagonal_elements(n, dNum=2, eNum=-1)
+def tridiagonal(n, dNum=2, eNum=-1):
     """
     Generates the diagonal elements of a tridiagonal [nxn] matrix
     with identical elements along the diagonal as two vectors.
     """
-    if n > 4:  # Matrix does not make sense for n>4
+    if n < 4:  # Matrix does not make sense for n>4
         raise ValueError("n too small")
 
     d = np.ones(n) * dNum
@@ -23,3 +30,28 @@ def multiply(A, v):
     d = [d, e] where d: diagonal, e:next to diagonal
     v = n-dim vector
     """
+
+    if np.shape(A)[0] != len(v):
+        raise ValueError("Can not multiply due to dimensions.")
+
+    n = len(v)
+    m = np.shape(A)[1]
+
+    b = np.zeros(n)
+
+    for i in xrange(n):
+        for j in xrange(m):
+            b[i] += A[i][j] * v[i]
+
+    return b
+
+
+A = np.array([[1, 0, 0, 0],
+              [0, 1, 0, 0],
+              [0, 0, 1, 0],
+              [0, 0, 0, 1],
+              ])
+
+v = np.array([1, 2, 3, 4])
+
+print multiply(A, v)
