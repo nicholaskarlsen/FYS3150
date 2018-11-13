d9e796666a19910ad6e3e87ddc7c8bcb45b1a903
Nicholas Karlsen <nichoka@student.matnat.uio.no>
2018-11-13 18:53:48 +0100

    did more writing

--- Project_4/code/metropolis.py
+++ Project_4/code/metropolis.py
@@ -1,7 +1,23 @@
+# Python Version 2.7.15
+from __future__ import division  # Nobody expects the integer division
 import numpy as np
 
 
-def generateState(N):
+def generateState(N, spin=False):
     # function wrapper which returns [N, N] array of random values in [-1, 1]
-    array = np.random.choice([-1, 1], size=(N, N))
+    # unless the spin parameter is set, in which case it will return a filled array
+    if spin is False:
+        array = np.random.choice([-1, 1], size=(N, N))
+    else:
+        array = np.full(shape=(N, N), fill_value=spin)
+
     return array
+
+
+def main():
+
+    return
+
+
+if __name__ == '__main__':
+    main()
